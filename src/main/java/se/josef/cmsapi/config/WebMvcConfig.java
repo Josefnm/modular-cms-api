c178ab58c0af7b425a954b63fbe62ed96e671a71
josef <josefnm86@gmail.com>
2020-04-17 18:14:49 +0200

    testing cross origin

--- src/main/java/se/josef/cmsapi/config/WebMvcConfig.java
+++ src/main/java/se/josef/cmsapi/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/users/")
+            registry.addMapping("/users")
                     .allowedOrigins("http://localhost:3000")
                     .allowedMethods("*");
         }
