0f2afca928f49dec3ee919e2c187f3828061942f
josef <josefnm86@gmail.com>
2020-04-17 13:56:00 +0200

    Added some comments and reorganized kubernetes config files

--- src/main/java/se/josef/cmsapi/model/document/User.java
+++ src/main/java/se/josef/cmsapi/model/document/User.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
 
     private String id;
+    // Firebase id
     private String fireId;
     private String userName;
     private String email;
