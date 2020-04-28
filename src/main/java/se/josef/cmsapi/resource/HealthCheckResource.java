4b64d7d51a7799e642e52f91abcc7868ad7702c6
josef <josefnm86@gmail.com>
2020-04-28 16:12:59 +0200

    fixed helath probe path

--- src/main/java/se/josef/cmsapi/resource/HealthCheckResource.java
+++ src/main/java/se/josef/cmsapi/resource/HealthCheckResource.java
@@ -3,12 +3,8 @@ package se.josef.cmsapi.resource;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
-@RequestMapping(value = "/api")
 @RestController
 public class HealthCheckResource {
 
