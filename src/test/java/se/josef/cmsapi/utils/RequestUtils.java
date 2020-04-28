6263fab222745cb30f70951b1fd2faf40c11fda2
josef <josefnm86@gmail.com>
2020-04-28 17:04:21 +0200

    fixed helath probe path

--- src/test/java/se/josef/cmsapi/utils/RequestUtils.java
+++ src/test/java/se/josef/cmsapi/utils/RequestUtils.java
@@ -21,7 +21,7 @@ public class RequestUtils {
     public <T> ResponseEntity<T> httpRequest(String path, HttpMethod httpMethod, Class<T> responseType, T body, int port) {
         return restTemplate
                 .exchange(
-                        "http://localhost:" + "/api" + path,
+                        "http://localhost:" +port+ "/api" + path,
                         httpMethod,
                         new HttpEntity<>(body),
                         responseType
